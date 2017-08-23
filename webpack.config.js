8dc6d4f347845083d1b6a378a582e608f16234a6
Jonathan Wyse <jonathan.wyse@pearson.com>
2017-08-23 10:23:07 -0600

    build working

--- webpack.config.js
+++ webpack.config.js
@@ -30,16 +30,16 @@ const imgPearsonLogo         = `${__dirname}/src/styles/assets/images/PearsonLog
 
 module.exports = {
   entry: {
-    elements          : [ elements, osItalicWoff, osItalicWoffII, osLightWoff, osLightWoffII, osLightItalicWoff, osLightItalicWoffII, osRegularWoff, osRegularWoffII, osSemiBoldWoff, osSemiBoldWoffII, osSemiBoldItalicWoff, osSemiBoldItalicWoffII, imgDarkOctocat, imgPearsonSprite, imgPearsonIcon, imgPearsonLogo ],
-    elementsNoPlain   : [ elementsNP ],
-    demo              : [ demo, demoScss ],
-    dev               : [ icons ],
-    eventInstantiator : [ main ],
-    dist              : [ compounds ]
+    'elements'          : [ elements, osItalicWoff, osItalicWoffII, osLightWoff, osLightWoffII, osLightItalicWoff, osLightItalicWoffII, osRegularWoff, osRegularWoffII, osSemiBoldWoff, osSemiBoldWoffII, osSemiBoldItalicWoff, osSemiBoldItalicWoffII, imgDarkOctocat, imgPearsonSprite, imgPearsonIcon, imgPearsonLogo ],
+    'elementsNoPlain'   : [ elementsNP ],
+    'demo.compounds'              : [ demo, demoScss ],
+    'dev.compounds'               : [ icons ],
+    'eventInstantiator' : [ main ],
+    'dist.compounds'              : [ compounds ]
   },
   output: {
     path          : path.resolve(__dirname, 'build'),
-    filename      : '[name].compounds.js',
+    filename      : '[name].[ext]',
     publicPath    : '/build/',
     libraryTarget : 'umd'
   },
