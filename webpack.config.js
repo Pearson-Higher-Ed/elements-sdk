cdc228a4698a26dcec3c5bd9761a87be9e114957
Jonathan Wyse <jonathan.wyse@pearson.com>
2017-08-23 10:21:35 -0600

    build working

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,37 @@
-const path              = require('path');
-const webpack           = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const demo              = `${__dirname}/demo/demo.js`;
-const demoScss          = `${__dirname}/demo/demo.scss`;
-const main              = `${__dirname}/demo/main.js`;
-const compounds         = `${__dirname}/index.js`;
-const icons             = `${__dirname}/src/styles/assets/icons/p-icons-sprite-1.1.svg`;
-
+const path                   = require('path');
+const webpack                = require('webpack');
+const HtmlWebpackPlugin      = require('html-webpack-plugin');
+const ExtractTextPlugin      = require('extract-text-webpack-plugin');
+const extractCSS             = new ExtractTextPlugin('css/[name].css');
+const extractSCSS            = new ExtractTextPlugin('css/[name].css');
+const demo                   = `${__dirname}/demo/demo.js`;
+const demoScss               = `${__dirname}/demo/demo.scss`;
+const main                   = `${__dirname}/demo/main.js`;
+const compounds              = `${__dirname}/index.js`;
+const elements               = `${__dirname}/src/styles/elements.scss`;
+const elementsNP             = `${__dirname}/src/styles/elementsNoPlain.scss`;
+const icons                  = `${__dirname}/src/styles/assets/icons/p-icons-sprite-1.1.svg`;
+const osItalicWoff           = `${__dirname}/src/styles/assets/fonts/opensans-italic.woff`;
+const osItalicWoffII         = `${__dirname}/src/styles/assets/fonts/opensans-italic.woff2`;
+const osLightWoff            = `${__dirname}/src/styles/assets/fonts/opensans-light.woff`;
+const osLightWoffII          = `${__dirname}/src/styles/assets/fonts/opensans-light.woff2`;
+const osLightItalicWoff      = `${__dirname}/src/styles/assets/fonts/opensans-lightitalic.woff`;
+const osLightItalicWoffII    = `${__dirname}/src/styles/assets/fonts/opensans-lightitalic.woff2`;
+const osRegularWoff          = `${__dirname}/src/styles/assets/fonts/opensans-regular.woff`;
+const osRegularWoffII        = `${__dirname}/src/styles/assets/fonts/opensans-regular.woff2`;
+const osSemiBoldWoff         = `${__dirname}/src/styles/assets/fonts/opensans-semibold.woff`;
+const osSemiBoldWoffII       = `${__dirname}/src/styles/assets/fonts/opensans-semibold.woff2`;
+const osSemiBoldItalicWoff   = `${__dirname}/src/styles/assets/fonts/opensans-semibolditalic.woff`;
+const osSemiBoldItalicWoffII = `${__dirname}/src/styles/assets/fonts/opensans-semibolditalic.woff2`;
+const imgDarkOctocat         = `${__dirname}/src/styles/assets/images/dark-octocat.svg`;
+const imgPearsonSprite       = `${__dirname}/src/styles/assets/images/Pearson-sprite.svg`;
+const imgPearsonIcon         = `${__dirname}/src/styles/assets/images/PearsonIcon.svg`;
+const imgPearsonLogo         = `${__dirname}/src/styles/assets/images/PearsonLogo.svg`;
 
 module.exports = {
   entry: {
+    elements          : [ elements, osItalicWoff, osItalicWoffII, osLightWoff, osLightWoffII, osLightItalicWoff, osLightItalicWoffII, osRegularWoff, osRegularWoffII, osSemiBoldWoff, osSemiBoldWoffII, osSemiBoldItalicWoff, osSemiBoldItalicWoffII, imgDarkOctocat, imgPearsonSprite, imgPearsonIcon, imgPearsonLogo ],
+    elementsNoPlain   : [ elementsNP ],
     demo              : [ demo, demoScss ],
     dev               : [ icons ],
     eventInstantiator : [ main ],
@@ -54,14 +76,21 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(css|scss)$/,
-        use: [{
-          loader: 'style-loader' // creates style nodes from JS strings
-        }, {
-          loader: 'css-loader' // translates CSS into CommonJS
-        }, {
-          loader: 'sass-loader' // compiles Sass to CSS
-        }]
+        test: /\.scss$/,
+        use: extractSCSS.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'sass-loader'
+          ]
+        })
+      },
+      {
+        test: /\.css$/,
+        use: extractCSS.extract([
+          'css-loader',
+          'style-loader'
+        ])
       },
       {
         test: /\.(js|jsx)$/,
@@ -76,11 +105,18 @@ module.exports = {
         }
       },
       {
-        test: /\.(png|jpg|gif|svg)$/,
+        test: /\/icons\/\S+\.svg$/,
         loader: 'file-loader',
         options: {
           name: '/icons/[name].[ext]?[hash]'
         }
+      },
+      {
+        test: /\/images\/\S+\.(gif|jpg|png|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '/images/[name].[ext]?[hash]'
+        },
       }
     ]
   },
@@ -91,6 +127,8 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV' : JSON.stringify(process.env.NODE_ENV)
     }),
-    new webpack.NamedModulesPlugin()
+    new webpack.NamedModulesPlugin(),
+    extractSCSS,
+    extractCSS
   ]
 };
