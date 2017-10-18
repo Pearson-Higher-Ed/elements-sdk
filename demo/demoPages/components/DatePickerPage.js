634eea4e4818e2eac49cf7fbb1a37f29b16adcc4
Michael <mhomolak@yahoo.com>
2017-10-18 12:29:49 -0600

    chore: pull in more changes

--- demo/demoPages/components/DatePickerPage.js
+++ demo/demoPages/components/DatePickerPage.js
@@ -73,7 +73,7 @@ class DatePickerPage extends Component {
               infoMessage     = {text.textInputInfoMessage}
               errorMessage    = {text.textInputErrorMessage}
             />
-            <p className="code">{`<DatePicker id = "someGiantId" dateFormat = "mm/dd/yyyy" inputState = "default" labelText = "Select date" datepickerValue = {this.state.datepickerValue2} changeHandler = {() => console.log("DatePicker-(basic)-changed!!")} infoMessage = "${text.textInputInfoMessage}" errorMessage = "${text.textInputErrorMessage}" />`}</p>
+            <p className="code">{`<DatePicker id = "someGiantId" dateFormat = "dd/mm/yyyy" inputState = "default" labelText = "Select date" datepickerValue = {this.state.datepickerValue2} changeHandler = {() => console.log("DatePicker-(basic)-changed!!")} infoMessage = "${text.textInputInfoMessage}" errorMessage = "${text.textInputErrorMessage}" />`}</p>
 
             <h2>DatePicker (range): </h2>
             <DatePicker
