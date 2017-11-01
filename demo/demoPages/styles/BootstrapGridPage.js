760788b65dd1886151d1dff93a611a72570776c2
Michael <mhomolak@yahoo.com>
2017-11-01 15:26:20 -0600

    chore: Update text

--- demo/demoPages/styles/BootstrapGridPage.js
+++ demo/demoPages/styles/BootstrapGridPage.js
@@ -5,7 +5,7 @@ export default () => (
     <h2><a href="http://getbootstrap.com/docs/4.0/layout/grid/" target="_blank">Bootstrap - Grid</a></h2>
 
     <p>
-      We do not own this component, but because of its popularity v4.0 has been included in the Elements-SDK. <br/>
+      We do not own this component, but because of its popularity v4.0 of Bootstrap&#8217;s Grid has been included in the Elements-SDK. <br/>
       For more documentation on this component please visit the link above.
     </p>
   </div>
