33911547fb3a97e01ba0037969fcf1eafccafba9
Michael <mhomolak@yahoo.com>
2017-12-12 10:02:47 -0700

    chore: move const inside of function

--- demo/demoPages/components/FooterPage.js
+++ demo/demoPages/components/FooterPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Footer } from '../../../index';
 
-const currentYear = new Date().getFullYear();
 
 const FooterPage = () => {
+
+  const currentYear = new Date().getFullYear();
   const testLinks = [{
     text: 'First link',
     href: 'first'
