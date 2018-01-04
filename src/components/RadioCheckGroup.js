6e4f38f85d600237b8c6981d16540ee7cb30eb4a
Jack Mallahan <jackmallahan@gmail.com>
2018-01-04 12:46:13 -0700

    Update RadioCheckGroup.js

--- src/components/RadioCheckGroup.js
+++ src/components/RadioCheckGroup.js
@@ -1,41 +1,39 @@
-import React     from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import PropTypes            from 'prop-types';
 
 
-const RadioCheckGroup = ({ legendText, options, name, inputType, selectedOptions, changeHandler }) => {
-
-  return (
-        <fieldset className="pe-fieldset">
-          <legend className="pe-legend">{legendText}</legend>
-          {Object.keys(options).map((opt, i) => {
-            return (
-                  <div key={`${name}-${opt}-${i}`} className={(inputType === 'radio')?"pe-radio":"pe-checkbox"}>
-                    <input id       = {`radiocheck-${name}-${opt}-${i}`}
-                           type     = {inputType}
-                           name     = {name}
-                           value    = {opt}
-                           checked  = {selectedOptions.indexOf(opt) > -1}
-                           disabled = {(inputType === 'checkbox') ? options[opt].toLowerCase().indexOf('disabled') > -1 : (Object.keys(options).filter(o => options[o] === 'disabled').length > 0) }
-                           onChange = {changeHandler}
-                           />
-                    <label htmlFor={`radiocheck-${name}-${opt}-${i}`}>{opt}</label>
-                    <span>
-                      <svg
-                        aria-hidden = "true"
-                        focusable   = "false"
-                        className   = {inputType==='radio'?"pe-icon--radio-dot":"pe-icon--check-sm-18"}
-                        >
-                        <use xlinkHref={inputType==='radio'?"#new-notification-9":"#check-sm-18" }></use>
-                      </svg>
-                    </span>
-                  </div>
-                )
-            })
-          }
-        </fieldset>
-    )
-  }
-
+const RadioCheckGroup = ({ legendText, options, name, inputType, changeHandler }) => {
+    return (
+          <fieldset className="pe-fieldset">
+            <legend className="pe-legend">{legendText}</legend>
+            {options.map((opt, i) => {
+              return (
+                    <div key={`${name}-${opt.value}-${i}`} className={(inputType === 'radio')?"pe-radio":"pe-checkbox"}>
+                      <input id       = {`radiocheck-${name}-${opt.value}-${i}`}
+                             type     = {inputType}
+                             name     = {name}
+                             value    = {opt.value}
+                             checked  = {opt.checked}
+                             disabled = {opt.disabled}
+                             onChange = {changeHandler}
+                             />
+                      <label htmlFor={`radiocheck-${name}-${opt.value}-${i}`}>{opt.label}</label>
+                      <span>
+                        <svg
+                          aria-hidden = "true"
+                          focusable   = "false"
+                          className   = {inputType==='radio'?"pe-icon--radio-dot":"pe-icon--check-sm-18"}
+                          >
+                          <use xlinkHref={inputType==='radio'?"#new-notification-9":"#check-sm-18" }></use>
+                        </svg>
+                      </span>
+                    </div>
+                  )
+              })
+            }
+          </fieldset>
+      )
+    }
 
 export default RadioCheckGroup;
 
@@ -43,8 +41,8 @@ export default RadioCheckGroup;
 RadioCheckGroup.propTypes = {
   id              : PropTypes.string.isRequired,
   legendText      : PropTypes.string.isRequired,
-  options         : PropTypes.object.isRequired,
+  options         : PropTypes.array.isRequired,
   inputType       : PropTypes.string.isRequired,
   changeHandler   : PropTypes.func.isRequired,
-  selectedOptions : PropTypes.array
+  name            : PropTypes.string,
 };
