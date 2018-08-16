593bbd7ce74a2831ef85df8174bb346cc9aa4e2e
Jakob, Jacqueline <Jackie.Jakob@Pearson.com>
2018-08-15 17:58:19 -0700

    remove references to elements.scss from components and fix dropdown bug

--- src/components/Dropdown/DropdownItem.js
+++ src/components/Dropdown/DropdownItem.js
@@ -30,7 +30,7 @@ const DropdownItem = ({
         break;
       case 'link':
         return (
-          <li role="presentation" data-item={label} key={`${dropdownId}${selectValue}`}>
+          <li role="presentation" data-item={label} key={`${dropdownId}${selectValue}`} id={dropdownId + "-" + selectValue}>
             <a href={url} className={checkmark ? 'checkmark' : ''} role="menuitem" tabIndex="-1">{label}</a>
           </li>
         );
