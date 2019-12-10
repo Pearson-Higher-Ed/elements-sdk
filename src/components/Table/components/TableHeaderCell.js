528fe8f39f85472503a5f2b7b070597a6e5a6aa1
David O'Dey <dodey@icloud.com>
2019-12-10 11:37:51 -0700

    screen reader now reads the sort direction when table cells are clicked

--- src/components/Table/components/TableHeaderCell.js
+++ src/components/Table/components/TableHeaderCell.js
@@ -30,82 +30,102 @@ export default class TableHeaderCell extends Component {
     }
 
     this.selectAll = _selectAll.bind(this);
+    this.iconToggle = this.iconToggle.bind(this);
   }
 
   isControlled() {
     return this.props.iconName != null
   }
 
-  iconToggle = () => {
-    const { iconName } = this.state;
-    this.props.columnSort()
+  iconToggle = (event) => {
+    const { iconName } = this.isControlled() ? this.props : this.state;
+    const value = event.currentTarget.innerText + " " + event.currentTarget.title,
+      node = event.currentTarget.parentNode.parentNode.parentNode.parentNode,
+      span = document.createElement('div'),
+      nodeid = node.nextElementSibling.id;
 
-    if (iconName === 'sort-up-18') {
-      return this.setState({ iconName: 'sort-down-18' })
+    if (nodeid === 'allyAlert') {
+      node.nextSibling.remove();
     }
 
-    return this.setState({ iconName: 'sort-up-18' });
+    span.setAttribute('role', 'alert');
+    span.setAttribute('id', 'allyAlert');
+    span.style.position = 'absolute';
+    span.style.top = '-9999px';
+    span.style.left = '-9999px';
+    this.props.columnSort();
+
+    if (iconName === 'sort-up-18') {
+      span.innerHTML = event.currentTarget.innerText + " " + 'Sorted down';
+      node.parentNode.insertBefore(span, node.nextSibling);
+      return this.setState({ iconName: 'sort-down-18' })
+    } else if (iconName === 'sort-down-18') {
+      span.innerHTML = event.currentTarget.innerText + " " + 'Sorted Up';
+      node.parentNode.insertBefore(span, node.nextSibling);
+      return this.setState({ iconName: 'sort-up-18' });
+    }
   }
 
   render() {
+    console.log(this.props)
     const { children, cellId, inputId, labelledbyCellId, inputLabel, ariaLabel, columnSort,
-            alignCell, scope } = this.props;
+      alignCell, scope } = this.props;
     const { selectable, sortable } = this.context;
     const { iconName } = this.isControlled() ? this.props : this.state;
     const sortClass = sortable && columnSort ? 'pe-table__sortable' : '';
     const columnAlignment = (alignCell === 'center' || alignCell === 'right')
-                            ? ' pe-table__' + alignCell : '';
+      ? ' pe-table__' + alignCell : '';
     const sortCheck =  columnSort ? columnSort :null;
     const arialabel = ariaLabel
-                  ? ariaLabel
-                  : null;
-    const labelledby = labelledbyCellId 
-                      ? labelledbyCellId
-                      : null;
+      ? ariaLabel
+      : null;
+    const labelledby = labelledbyCellId
+      ? labelledbyCellId
+      : null;
 
     return (
       <th  id={cellId}
            aria-sort={
-            iconName === 'sort-up-18'
-            ? 'ascending'
-            : iconName === 'sort-down-18'
-            ? 'descending'
-            : iconName === 'sortable-18'
-            ? 'none'
-            : null}
-          columnSort={sortCheck}
-          className={
-            sortClass !='' || columnAlignment != ''
-            ? `${sortClass}${columnAlignment}`
-            : null}
-          scope={scope}
+             iconName === 'sort-up-18'
+               ? 'ascending'
+               : iconName === 'sort-down-18'
+               ? 'descending'
+               : iconName === 'sortable-18'
+                 ? 'none'
+                 : null}
+           columnSort={sortCheck}
+           className={
+             sortClass !='' || columnAlignment != ''
+               ? `${sortClass}${columnAlignment}`
+               : null}
+           scope={scope}
       >
         {
           selectable && !columnSort && !children
             ? <div className="pe-checkbox"
                    onClick={
                      scope === 'col'
-                     ? this.selectAll
-                     : null
-                     }>
-                <input type="checkbox" id={inputId} aria-labelledby={labelledby} aria-label={arialabel}/>
-                <label htmlFor={inputId}>{inputLabel}</label>
-                <span>
+                       ? this.selectAll
+                       : null
+                   }>
+              <input type="checkbox" id={inputId} aria-labelledby={labelledby} aria-label={arialabel}/>
+              <label htmlFor={inputId}>{inputLabel}</label>
+              <span>
                   <Icon name="check-sm-18" />
                 </span>
-              </div>
+            </div>
             : children
         }
         {
-          columnSort && 
-            <button type="button" title={
-              iconName === 'sort-up-18'
+          columnSort &&
+          <button type="button" title={
+            iconName === 'sort-up-18'
               ? 'Sorted up'
               : iconName === 'sort-down-18'
               ? 'Sorted down'
               : 'Unsorted' } onClick={this.iconToggle}>
-              {inputLabel}<Icon name={iconName} />
-            </button>
+            {inputLabel}<Icon name={iconName} />
+          </button>
         }
       </th>
     )
@@ -122,15 +142,15 @@ function _selectAll() {
 
   tables.forEach((table) => {
     let thead = table.getElementsByTagName('thead')[0],
-        tbody = table.getElementsByTagName('tbody')[0];
+      tbody = table.getElementsByTagName('tbody')[0];
     let thInput = thead.getElementsByTagName('INPUT')[0],
-        trs = [].slice.call(tbody.getElementsByTagName('TR'));
+      trs = [].slice.call(tbody.getElementsByTagName('TR'));
     let trInputs = trs.map(tr => tr.getElementsByTagName('INPUT')[0]);
 
     if (thInput && thInput.type === 'checkbox') {
-        trInputs.forEach((input) => {
-          input.checked = thInput.checked;
-        });
-    }   
+      trInputs.forEach((input) => {
+        input.checked = thInput.checked;
+      });
+    }
   });
 }
